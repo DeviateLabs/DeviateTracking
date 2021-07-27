bb06de64612a9a0f65a13326a331642984c381c7
StructByLightning <mossygreenrock@gmail.com>
2021-07-27 13:12:59 -0700

    Add test for sendServerEvent url encoding

--- src/server/sendServerEvent/index.test.js
+++ src/server/sendServerEvent/index.test.js
@@ -30,6 +30,11 @@ describe("sendServerEvent", () => {
 
   it("sends the data query arg", async() => {
     sendServerEvent({testkey: "testval"});
-    expect(global.fetch).toHaveBeenCalledWith(expect.stringMatching(/data={\"testkey\":\"testval\"}/));
+    expect(global.fetch).toHaveBeenCalledWith(expect.stringMatching(/data=%7B%22testkey%22%3A%22testval%22%7D/));
+  });
+
+  it("urlencodes the data query arg", async() => {
+    sendServerEvent({testkey: "t estval"});
+    expect(global.fetch).toHaveBeenCalledWith(expect.stringMatching(/data=%7B%22testkey%22%3A%22t%20estval%22%7D/));
   });
 });
