110a3a895b7b5313b335bd0bdfb6e02db97845d2
StructByLightning <mossygreenrock@gmail.com>
2021-11-01 15:28:55 -0700

    Fix versioning

--- src/server/config.js
+++ src/server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  version: "5.2.2",
+  version: "5.3.1",
   capiUrl: "https://v5.api.deviatetracking.com/capi?data={{DATA}}",
   //capiUrl: "http://localhost:4080/capi?data={{DATA}}",
 };
